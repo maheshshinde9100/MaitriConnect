fc5468c650b9e56bef0d8b4c5fcbb4c5d29da714
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-10-19 19:33:53 +0530

    intial commit for backend of my chat application - MaitriConnect

---
+++ backend/src/test/java/com/chatapp/maitriconnect/MaitriconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.maitriconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaitriconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
