1c5c2dfacb60feeec47a336c498748abae595b0f
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-10-19 23:51:03 +0530

    excepts dir

---
+++ backend/auth-service/src/main/java/com/maitriconnect/auth_service/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.maitriconnect.auth_service.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
