5b8d2deb53a5a67af22138dcb9bff958ebbacfa5
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-10-24 21:23:39 +0530

    EnableDiscoveryClient in chat-service

--- backend/chat-service/src/main/java/com/maitriconnect/chat_service/ChatServiceApplication.java
+++ backend/chat-service/src/main/java/com/maitriconnect/chat_service/ChatServiceApplication.java
@@ -2,8 +2,10 @@ package com.maitriconnect.chat_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
