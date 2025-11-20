55cb965532180f36606b3ea0eeb43bc6ebe01fe4
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-11-20 20:29:59 +0530

    service discovery -- changes in properties file - changed the configsc

--- backend/service-discovery/src/test/java/com/maitriconnect/service_discovery/ServiceDiscoveryApplicationTests.java
+++ backend/service-discovery/src/test/java/com/maitriconnect/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -1,13 +1,13 @@
-package com.maitriconnect.service_discovery;
+// package com.maitriconnect.service_discovery;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ServiceDiscoveryApplicationTests {
+// @SpringBootTest
+// class ServiceDiscoveryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
